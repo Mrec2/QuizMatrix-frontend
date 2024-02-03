9af016ecc6d2cda57efb012034a5596ca4321f4e
Juan Manuel Iriondo Ortega <juanmanuel.iriondo@gmail.com>
2024-02-03 18:04:33 +0100

    primera aproximacion a login preparada

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,74 +1,11 @@
-import React, { Component } from "react";
+import LoginComp from "../../components/LoginComp/LoginComp";
 
-import "./Login.css";
+const Login = () => {
+  return (
+    <>
+      <LoginComp />
+    </>
+  );
+};
 
-
-export default class Login extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            usuario: "",
-            password: "",
-            errorText: "El usuario no es correcto"
-        }
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value,
-            errorText: ""
-        });
-    }
-
-    handleSubmit() {
-
-    }
-
-    render() {
-
-        return (
-
-            <div className="login">
-                <div className="login-contenedor">
-                    <div className="login-contenedor-texto">
-                        <h2>LOGIN</h2>
-                    </div>
-
-                    <div className="login-contenedor-error">{this.state.errorText}</div>
-
-                    <form className="login-contenedor-form" onSubmit={this.handleSubmit}>
-                        <div className="one-column">
-                            <input
-                                type="text"
-                                name="usuario"
-                                required autoFocus
-                                placeholder="Usuario"
-                                value={this.state.usuario}
-                                onChange={this.handleChange}
-                            />
-                        </div>
-                        <div className="one-column">
-                            <input
-                                type="password"
-                                name="password"
-                                required
-                                placeholder="Password"
-                                value={this.state.password}
-                                onChange={this.handleChange}
-                            />
-                        </div>
-
-                        <div className="btn-contenedor">
-                            <button className="btn" type="submit">Login</button>
-                        </div>
-                    </form>
-                </div>    
-            </div>
-        )
-    }
-}
+export default Login;
--- src/router/RouterApps.jsx
+++ src/router/RouterApps.jsx
@@ -3,7 +3,7 @@ import Homepage from "../pages/Homepage/Homepage.jsx";
 import Contact from "../pages/Contact/Contact.jsx";
 import Account from "../pages/Account/Account.jsx";
 
-
+import Login from "../pages/Login/Login.jsx";
 
 const AppRoutes = () => {
   return (
@@ -11,6 +11,8 @@ const AppRoutes = () => {
       <Route path="/" element={<Homepage />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/account" element={<Account />} />
+
+      <Route path="/register" element={<Login />} />
     </Routes>
   );
 };
