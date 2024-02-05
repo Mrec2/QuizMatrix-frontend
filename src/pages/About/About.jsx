0cc8600aad7ec9c03f6b297de68cd5e670cd7b34
Juan Manuel Iriondo Ortega <juanmanuel.iriondo@gmail.com>
2024-02-05 18:31:04 +0100

    preparando componente about

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,11 @@
+import AboutComp from "../../components/AboutComp/AboutComp";
+
+const About = () => {
+  return (
+    <>
+      <AboutComp />
+    </>
+  );
+};
+
+export default About;
--- src/router/RouterApps.jsx
+++ src/router/RouterApps.jsx
@@ -1,17 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Homepage from "../pages/Homepage/Homepage.jsx";
+import About from "../pages/About/About.jsx";
 import Contact from "../pages/Contact/Contact.jsx";
 import Account from "../pages/Account/Account.jsx";
-
 import Login from "../pages/Login/Login.jsx";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
+      <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/account" element={<Account />} />
-
       <Route path="/register" element={<Login />} />
     </Routes>
   );
