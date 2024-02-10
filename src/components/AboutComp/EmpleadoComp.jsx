02053b18247b4126e69b0eec89c71cb898b12122
Juan Manuel Iriondo Ortega <juanmanuel.iriondo@gmail.com>
2024-02-10 22:07:20 +0100

    actualizando código en [about]

---
+++ src/components/AboutComp/EmpleadoComp.jsx
@@ -0,0 +1,35 @@
+import { Component } from "react";
+
+import "./EmpleadoComp.css";
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      empleado: this.props.empleado,
+      descripcion: this.props.descripcion,
+      foto: this.props.foto,
+      linkedin:""
+    };
+
+  }
+
+  render() {
+
+    const fotoEmpleado = "../../../img/about/" + this.state.foto + ".jpg";
+
+    return (
+      <div className="empleado">
+
+        <div className="empleado-foto">
+            <img src={fotoEmpleado} alt="foto de empleado" />
+        </div>
+
+        <div className="empleado-nombre">{this.state.empleado}</div>
+        <div className="empleado-descripcion">{this.state.descripcion}</div>
+        <div className="empleado-linkedin"></div>
+      </div>
+    );
+  }
+}
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,4 +1,7 @@
+import "./About.css";
+
 import AboutComp from "../../components/AboutComp/AboutComp";
+import EmpleadoComp from "../../components/AboutComp/EmpleadoComp";
 
 const About = () => {
   return (
@@ -6,6 +9,23 @@ const About = () => {
       <AboutComp />
       <AboutComp />
       <AboutComp />
+
+      <div className="about-empleados">
+
+        <div className="about-empleado">
+          <EmpleadoComp empleado="Mario" descripcion="FullStack Developer" foto="mario" />
+        </div>
+
+        <div className="about-empleado">
+          <EmpleadoComp empleado="Alberto" descripcion="Frontend Developer" foto="juanma" />
+        </div>
+          
+        <div className="about-empleado">
+          <EmpleadoComp empleado="Juanma" descripcion="Backend Developer" foto="juanma" />
+        </div>
+
+      </div>
+
     </>
   );
 };
