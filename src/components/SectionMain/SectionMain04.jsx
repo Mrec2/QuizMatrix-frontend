748ca738d40c7a62a5b98b9ac28516e2916b3194
soycochises <soycochises@gmail.com>
2024-02-10 23:50:45 +0100

    Modification of root styles and creation of home page sections

---
+++ src/components/SectionMain/SectionMain04.jsx
@@ -0,0 +1,23 @@
+import "./SectionMain04.css";
+
+const SectionMain04 = () => {
+    return (
+        <div className="contenedor">
+            <section className="seccion-principal">
+                <div>
+                    <h3 className="seccion-info--title">Todos los tests puedes realizarlos en cualquier dispositivo, desde ordenadores hasta dispositivos móviles.</h3>
+                </div>
+                <div>
+                    <img
+                        src="../imgs-home-page/computer-icon.png"
+                        alt="Icono de Ordenador y Movil"
+                        className="seccion-imagen-icon"
+                    />
+                </div>
+            </section>
+
+        </div>
+    );
+};
+
+export default SectionMain04;
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,14 +1,16 @@
 import "./Homepage.css";
 import SectionMain01 from "../../components/SectionMain/SectionMain01";
 import SectionMain02 from "../../components/SectionMain/SectionMain02";
+import SectionMain03 from "../../components/SectionMain/SectionMain03";
+import SectionMain04 from "../../components/SectionMain/SectionMain04";
 
 const Homepage = () => {
   return (
     <>
-      <h1>Bienvenido a TESTAPP</h1>
       <SectionMain01 />
-      <hr />
       <SectionMain02 />
+      <SectionMain03 />
+      <SectionMain04 />
     </>
   );
 };
